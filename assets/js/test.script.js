24d9f7c67fa864ae2014ce70fcf2b147d83fd8c5
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-08-01 15:56:45 +0100

    red 'score key exists'

--- assets/js/test.script.js
+++ assets/js/test.script.js
@@ -1,3 +1,15 @@
 /**
  * @jest-environment jsdom
  */
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs. readFileSync("index.html", "utf-8");
+    document.body.innerHTML = fileContents;
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+});
